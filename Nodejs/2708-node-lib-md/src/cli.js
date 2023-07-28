0af7e301ed606e56b5a0f134c18bff09d0c99392
IgorLucasBrazz <igor.braz@ufv.br>
2023-07-28 15:53:46 -0300

    Aula 4

--- Nodejs/2708-node-lib-md/src/cli.js
+++ Nodejs/2708-node-lib-md/src/cli.js
@@ -1,11 +1,37 @@
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
+import fs from 'fs';
+
 
 const caminho = process.argv;
 
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2])
-    console.log(chalk.yellow('lista de links'), resultado);
+function imprimeLista(resultado, identificador = '' ){
+    console.log(
+        chalk.yellow('lista de links'),
+        chalk.black.bgGreen(identificador),
+        resultado);
+}
+
+async function processaTexto(argumentos){
+    const caminho = argumentos[2];
+
+    try{
+        fs.lstatSync(caminho);
+    }catch(erro){
+        console.log(chalk.red('arquivo ou dirtorio não existe!!!'));
+        return;
+    }
+
+    if (fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async(nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(lista, nomeDeArquivo);
+        })
+    }
 }
 
 processaTexto(caminho);
